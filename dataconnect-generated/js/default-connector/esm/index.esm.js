7ebda4671ea9e9e24410eceb6764953033f1a1c4
Chremaghost <julia.goudalo@epitech.eu>
2024-11-15 14:37:21 +0100

    oul

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'Global-Plus-Vision',
+  location: 'us-central1'
+};
+
